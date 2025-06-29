ef853a5e3c13d794daab53ece02b3b85c2b1c34f
SaeedMolaiy <saeedmolaiy.dev@gmail.com>
2025-06-29 20:28:33 +0330

    Initializing project + main window basic style

---
+++ src/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Calculator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DragWindow(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void Minimize(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+        private void Maximize(object sender, RoutedEventArgs e) =>
+            WindowState = (WindowState == WindowState.Normal) ? WindowState.Maximized : WindowState.Normal;
+
+        private void Close(object sender, RoutedEventArgs e) => Close();
+    }
+}
