cb5b9d6ca7f18f5670fe8dba3be1c1a208963411
SaeedMolaiy <saeedmolaiy.dev@gmail.com>
2025-07-01 22:28:19 +0330

    Added keyboard support + some refactorings

--- src/Calculator/MainWindow.xaml.cs
+++ src/Calculator/MainWindow.xaml.cs
@@ -1,19 +1,56 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace Calculator
 {
     public partial class MainWindow : Window
     {
+        private const string NumberButtonsDefaultColor = "#3a3a3b";
+        private const string NumberButtonsClickColor = "#333233";
+
+        private const string EqualButtonDefaultColor = "#4dc2fe";
+        private const string EqualButtonClickColor = "#1b9cd8";
+
+        private const string OtherButtonsDefaultColor = "#333233";
+        private const string OtherButtonsClickColor = "#3a3a3b";
+
         private const char DecimalSeparator = '.';
 
         private bool haveDecimal = false;
         private MathOperation mathOperation;
 
+        private readonly ColorConverter _colorConverter;
+        private readonly RoutedEventArgs _clickEventArgs;
+
+        private readonly Dictionary<decimal, Button> _numberToButtonMap;
+
         public MainWindow()
         {
             InitializeComponent();
-            SetEventHandlers();
+
+            _colorConverter = new ColorConverter();
+            _clickEventArgs = new RoutedEventArgs(Button.ClickEvent);
+
+            _numberToButtonMap = new Dictionary<decimal, Button>()
+            {
+                {1, NumberOneButton },
+                {2, NumberTwoButton },
+                {3, NumberThreeButton },
+                {4, NumberFourButton },
+                {5, NumberFiveButton },
+                {6, NumberSixButton },
+                {7, NumberSevenButton },
+                {8, NumberEightButton },
+                {9, NumberNineButton },
+                {0, NumberZeroButton }
+            };
+
+            ConfigureWindowEventHandlers();
+            ConfigureMathOperationEventHandlers();
+            ConfigureOtherEventHandlers();
+            ConfigureNumberButtons();
         }
 
         private decimal CurrentNumber
@@ -56,36 +93,41 @@ namespace Calculator
             mathOperation = operation;
         }
 
-        private void SetEventHandlers()
+        private void ConfigureWindowEventHandlers()
         {
-            //Window
             CloseButton.Click += CloseButton_Click;
             MinimizeButton.Click += MinimizeButton_Click;
             MaximizeButton.Click += MaximizeButton_Click;
             TitleBar.MouseLeftButtonDown += TitleBarControl_MouseLeftButtonDown;
+            PreviewKeyDown += HandleKeyboardInput;
+        }
 
-            // Math Operations
+        private void ConfigureMathOperationEventHandlers()
+        {
             PlusButton.Click += PlusButton_Click;
             MinusButton.Click += MinusButton_Click;
             DivideButton.Click += DivideButton_Click;
             MultiplyButton.Click += MultiplyButton_Click;
             EqualButton.Click += EqualButton_Click;
+        }
 
-            // Numbers
-            NumberOneButton.Click += NumberOneButton_Click;
-            NumberTwoButton.Click += NumberTwoButton_Click;
-            NumberThreeButton.Click += NumberThreeButton_Click;
-            NumberFourButton.Click += NumberFourButton_Click;
-            NumberFiveButton.Click += NumberFiveButton_Click;
-            NumberSixButton.Click += NumberSixButton_Click;
-            NumberSevenButton.Click += NumberSevenButton_Click;
-            NumberEightButton.Click += NumberEightButton_Click;
-            NumberNineButton.Click += NumberNineButton_Click;
-            NumberZeroButton.Click += NumberZeroButton_Click;
-            DecimalButton.Click += DecimalButton_Click;
-
-            // Others
+        private void ConfigureOtherEventHandlers()
+        {
             ACButton.Click += ACButton_Click;
+            NegativeButton.Click += NegativeButton_Click;
+            DecimalButton.Click += DecimalButton_Click;
+        }
+
+        private void ConfigureNumberButtons()
+        {
+            foreach (var each in _numberToButtonMap)
+            {
+                var number = each.Key;
+                var button = each.Value;
+
+                button.Content = number.ToString();
+                button.Click += NumberButton_Click;
+            }
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e) => Close();
@@ -111,29 +153,15 @@ namespace Calculator
 
         private void EqualButton_Click(object sender, RoutedEventArgs e) => Calculate();
 
-        private void NumberOneButton_Click(object sender, RoutedEventArgs e) => AddNumber(1);
-
-        private void NumberTwoButton_Click(object sender, RoutedEventArgs e) => AddNumber(2);
-
-        private void NumberThreeButton_Click(object sender, RoutedEventArgs e) => AddNumber(3);
-
-        private void NumberFourButton_Click(object sender, RoutedEventArgs e) => AddNumber(4);
-
-        private void NumberFiveButton_Click(object sender, RoutedEventArgs e) => AddNumber(5);
-
-        private void NumberSixButton_Click(object sender, RoutedEventArgs e) => AddNumber(6);
-
-        private void NumberSevenButton_Click(object sender, RoutedEventArgs e) => AddNumber(7);
-
-        private void NumberEightButton_Click(object sender, RoutedEventArgs e) => AddNumber(8);
-
-        private void NumberNineButton_Click(object sender, RoutedEventArgs e) => AddNumber(9);
-
-        private void NumberZeroButton_Click(object sender, RoutedEventArgs e) => AddNumber(0);
-
         private void DecimalButton_Click(object sender, RoutedEventArgs e)
         {
-            if (haveDecimal)
+            var content =
+                ResultLabel.Content.ToString();
+
+            if (string.IsNullOrEmpty(content))
+                return;
+
+            if (haveDecimal && content.Contains(DecimalSeparator))
                 return;
 
             ResultLabel.Content = $"{CurrentNumber}{DecimalSeparator}";
@@ -145,5 +173,203 @@ namespace Calculator
             ResultLabel.Content = "0";
             haveDecimal = false;
         }
+
+        private void NegativeButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResultLabel.Content = (CurrentNumber * -1).ToString();
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+
+            if (button == null)
+                return;
+
+            var number =
+                GetButtonContentAsDecimal(button);
+
+            AddNumber(number);
+        }
+
+        private void HandleKeyboardInput(object sender, KeyEventArgs e)
+        {
+            var key = e.Key;
+
+            HandleNumberKeyPress(key);
+            HandleCopyAndPasteKeyPress(key);
+            HandleMathOperationsKeyPress(key);
+            HandleDecimalSeperatorKeyPress(key);
+            HandleBackSpaceKeyPress(key);
+
+            e.Handled = true;
+        }
+
+        private void HandleNumberKeyPress(Key key)
+        {
+            if (IsHoldingShift())
+                return;
+
+            if (TryParseNumberFromKeyboardKey(key, out var pressedNumber) == false)
+                return;
+
+            var clickButton =
+                _numberToButtonMap[pressedNumber];
+
+            if (clickButton != null)
+                _ = SimulateButtonClick(
+                    clickButton, NumberButtonsDefaultColor, NumberButtonsClickColor);
+
+            return;
+        }
+
+        private void HandleCopyAndPasteKeyPress(Key key)
+        {
+            if (IsHoldingControl() == false)
+                return;
+
+            if (key == Key.C)
+            {
+                Clipboard.SetText(CurrentNumber.ToString());
+            }
+            else if (key == Key.V)
+            {
+                if (Clipboard.ContainsText())
+                {
+                    var pastedText =
+                        Clipboard.GetText();
+
+                    if (decimal.TryParse(pastedText, out _))
+                    {
+                        ResultLabel.Content = pastedText;
+                    }
+                }
+            }
+        }
+
+        private void HandleMathOperationsKeyPress(Key key)
+        {
+            Button? clickedButton = null;
+
+            switch (key)
+            {
+                case Key.Enter:
+                    clickedButton = EqualButton;
+                    break;
+
+                case Key.OemPlus:
+                    clickedButton =
+                        IsHoldingShift() ? EqualButton : PlusButton;
+                    break;
+
+                case Key.OemMinus:
+                    clickedButton = MinusButton;
+                    break;
+
+                case Key.OemQuestion:
+                    clickedButton = DivideButton;
+                    break;
+
+                case Key.D8:
+                    if (IsHoldingShift())
+                        clickedButton = MultiplyButton;
+                    break;
+            }
+
+            if (clickedButton != null)
+            {
+                var defaultColor = OtherButtonsDefaultColor;
+                var color = OtherButtonsClickColor;
+
+                if (clickedButton.Equals(EqualButton))
+                {
+                    defaultColor = EqualButtonDefaultColor;
+                    color = EqualButtonClickColor;
+                }
+
+                _ = SimulateButtonClick(
+                    clickedButton, defaultColor, color);
+            }
+        }
+
+        private void HandleDecimalSeperatorKeyPress(Key key)
+        {
+            if (key == Key.OemPeriod)
+            {
+                _ = SimulateButtonClick(
+                    DecimalButton, NumberButtonsDefaultColor, NumberButtonsClickColor);
+            }
+        }
+
+        private void HandleBackSpaceKeyPress(Key key)
+        {
+            if (key == Key.Back)
+            {
+                var content =
+                    ResultLabel.Content.ToString() ?? "";
+
+                if (content.Length == 1)
+                    content = "0";
+                else
+                    content = content[..^1];
+
+                ResultLabel.Content = content;
+            }
+        }
+
+        private async Task SimulateButtonClick(Button button, string defaultColorHex, string colorHex)
+        {
+            button.RaiseEvent(_clickEventArgs);
+
+            var defaultColor = GetColorFromHex(defaultColorHex);
+            var color = GetColorFromHex(colorHex);
+
+            button.Background =
+                new SolidColorBrush(color);
+
+            await Task.Delay(120);
+
+            button.Background =
+                new SolidColorBrush(defaultColor);
+        }
+
+        private Color GetColorFromHex(string hex)
+        {
+            var convertedColor = _colorConverter.ConvertFrom(hex);
+
+            if (convertedColor != null)
+                return (Color)convertedColor;
+
+            return new Color();
+        }
+
+        private static bool TryParseNumberFromKeyboardKey(Key key, out decimal digit)
+        {
+            digit = key switch
+            {
+                >= Key.D0 and <= Key.D9 => key - Key.D0,
+                >= Key.NumPad0 and <= Key.NumPad9 => key - Key.NumPad0,
+                _ => -1
+            };
+
+            return digit >= 0;
+        }
+
+        private static decimal GetButtonContentAsDecimal(Button button)
+        {
+            var content =
+                button.Content.ToString();
+
+            if (string.IsNullOrEmpty(content))
+                return 0;
+
+            return decimal.Parse(content);
+        }
+
+        private static bool IsHoldingShift() =>
+             Keyboard.Modifiers.HasFlag(ModifierKeys.Shift);
+
+        private static bool IsHoldingControl() =>
+             Keyboard.Modifiers.HasFlag(ModifierKeys.Control);
     }
 }
